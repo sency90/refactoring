93c85bd41dc9eb9d002316e1fd25d210db4767d5
Donghyuk.Yang <sency90@gmail.com>
2025-07-17 12:47:33 +0900

    1_WheelOfFortune_KATA_TeacherCode 1st Commit

--- 0717/1_WheelOfFortune_KATA/Wheel.cpp
+++ 0717/1_WheelOfFortune_KATA/Wheel.cpp
@@ -4,7 +4,7 @@ using std::string;
 using std::vector;
 
 namespace{
-	constexpr int NOTYET = -1;
+	constexpr int DOLLAR_1000 = 1000;
 	constexpr int DOLLAR_2000 = 2000;
 };
 
@@ -29,72 +29,15 @@ int Wheel::getPrice(std::vector<std::string> quiz_lines,std::string user_ans) {
 
 	//하나씩 처리
 	//26글자 for 돌면서 퀴즈 참석자가 하나씩 시도를 하는 것
-	for(int z=0; z<user_ans.size(); z++) {
-		char user_ans_char = user_ans[z];
+	for(char user_ans_char : user_ans) {
 
 		//1. 2000 달러 찬스를 얻었는지 검사
 		prize += GetSecondChanceScore(quiz_lines, second_chance_by_lineno, is_flipped, user_ans_char);
-
-
-#if 0
-		int passCnt=0;
-		//2중 for 돌면서 정답 문자열을 하나씩 검사해서
-		//퀴즈참가자가 던진 문자가 존재하는지 검사
-		for(int lineno = 0; lineno < quiz_lines.size(); lineno++) {
-			std::string & quiz_line = quiz_lines[lineno];
-			for(int i = 0; i < quiz_line.size(); i++) {
-				if(is_flipped[lineno][i]) continue;
-
-				//만약 퀴즈참석자가 요청한 문자가, 
-				//정답문자열과 동일하다면
-				if(quiz_line[i] == user_ans_char) {
-
-					//Let's First 점수인지 확인한다.
-					if(ffirst[lineno] == 0 && i == 0) {
-						prize += 1000;
-						ffirst[lineno] = 1;
-						chance_by_lineno[lineno] = true;
-					}
-					else if(ffirst[lineno] == 0 && i != 0) {
-						ffirst[lineno] = 1;
-					}
-
-					//used배열
-					is_flipped[lineno][i] = true;
-
-					//동일한 문자 개수를 Counting한다.
-					passCnt++;
-				}
-			}
-		}
-#else
-
-#if 0
-		for(int lineno = 0; lineno < quiz_lines.size(); lineno++) {
-			std::string & quiz_line = quiz_lines[lineno];
-			for(int i = 0; i < quiz_line.size(); i++) {
-				if(is_flipped[lineno][i]) continue;
-				if(quiz_line[i] == user_ans_char) {
-					//Let's First 점수인지 확인한다.
-					if(first_chance_by_lineno[lineno] && i == 0) {
-						prize += 1000;
-						first_chance_by_lineno[lineno] = false;
-						second_chance_by_lineno[lineno] = true;
-					}
-					else if(first_chance_by_lineno[lineno] && i != 0) {
-						first_chance_by_lineno[lineno] = false;
-					}
-				}
-			}
-		}
-#else
 		prize += GetFirstChanceScore(quiz_lines, is_flipped, user_ans_char, first_chance_by_lineno, second_chance_by_lineno);
 		UpdateFirstAndSecondChance(quiz_lines, is_flipped, user_ans_char, first_chance_by_lineno, second_chance_by_lineno);
-#endif
 
 		int passCnt = GetPassCount(quiz_lines, is_flipped, user_ans_char);
 		UpdateFlipStatus(quiz_lines, is_flipped, user_ans_char);
-#endif
 
 		if(passCnt>0) {
 			conCnt++;
@@ -116,7 +59,7 @@ int Wheel::GetFirstChanceScore(std::vector<std::string> &quiz_lines, std::vector
 		if(is_flipped[lineno][0]) continue;
 		if(false == first_chance_by_lineno[lineno]) continue;
 
-		result += 1000;
+		result += DOLLAR_1000;
 	}
 	return result;
 }
