914d1a486e0e32bee9ba1a5ebb7b6c99a9b523e5
Donghyuk.Yang <sency90@gmail.com>
2025-07-17 12:56:12 +0900

    3rd commit (refactoring start)

--- 0717/1_WhellOfFortune_KATA_TeacherCode/Wheel.cpp
+++ 0717/1_WhellOfFortune_KATA_TeacherCode/Wheel.cpp
@@ -1,4 +1,3 @@
-#pragma once
 #include <string>
 #include <vector>
 
@@ -7,8 +6,8 @@ using std::vector;
 
 class Wheel {
 public:
-	int getPrice(vector<string> &strs, string userdata) {
-		int map[4][15] = {0};
+	int getPrice(const vector<string> &strs, string userdata) {
+		bool isFlip[4][15] = {false};
 
 		int conCnt = 0;
 		int ffirst[4] = {0};
@@ -20,57 +19,47 @@ public:
 		//26글자 for 돌면서 퀴즈 참석자가 하나씩 시도를 하는 것
 		for(int i = 0; i < 26; i++) {
 
+			char userAnswer = userdata[i];
+
 			//1. 2000 달러 찬스를 얻었는지 검사
 			for(int y = 0; y < strs.size(); y++) {
 				if(chance[y] != -1) {
 					for(int x = 0; x < strs[chance[y]].size(); x++) {
-						if(map[chance[y]][x] == 0 && strs[chance[y]][x] == userdata[i]) {
-							//획득 성공시 2000달러를 얻는다.
-							sum += 2000;
-							break;
-						}
+						if(isFlip[chance[y]][x]) continue;
+						if(strs[chance[y]][x] != userAnswer) continue;
+						//획득 성공시 2000달러를 얻는다.
+						sum += 2000;
+						break;
 					}
 					chance[y] = -1;
 				}
 			}
 
-			int flag = 0;
-			int passCnt = 0;
 
-			//2중 for 돌면서 정답 문자열을 하나씩 검사해서
-			//퀴즈참가자가 던진 문자가 존재하는지 검사
+			//Let's First 점수인지 확인한다.
 			for(int y = 0; y < strs.size(); y++) {
 				for(int x = 0; x < strs[y].size(); x++) {
-					if(map[y][x] == 1) continue;
+					if(isFlip[y][x]) continue;
+					if(strs[y][x] != userAnswer) continue;
 
-					//만약 퀴즈참석자가 요청한 문자가, 
-					//정답문자열과 동일하다면
-					if(strs[y][x] == userdata[i]) {
-
-						//Let's First 점수인지 확인한다.
-						if(ffirst[y] == 0 && x == 0) {
-							sum += 1000;
-							ffirst[y] = 1;
-							chance[y] = y;
-						}
-						else if(ffirst[y] == 0 && x != 0) {
-							ffirst[y] = 1;
-						}
-
-						//used배열
-						map[y][x] = 1;
-
-						//해당 문자를 _로 바꿔버린다.
-						strs[y][x] = '_';
-						flag = 1;
-
-						//동일한 문자 개수를 Counting한다.
-						passCnt++;
+					if(ffirst[y] == 0 && x == 0) {
+						sum += 1000;
+						ffirst[y] = 1;
+						chance[y] = y;
+					}
+					else if(ffirst[y] == 0 && x != 0) {
+						ffirst[y] = 1;
 					}
 				}
 			}
+			//동일한 문자 개수를 Counting한다.
+			int passCnt = getPassCount(strs, isFlip, userAnswer);
 
-			if(flag == 1) {
+
+			//뒤집었다면 체크해준다.
+			updateFlipStatus(strs, isFlip, userAnswer);
+
+			if(passCnt > 0) {
 				conCnt++;
 				sum += (conCnt * 100) * passCnt;
 			}
@@ -81,5 +70,27 @@ public:
 		}
 
 		return sum;
+	}
+
+	void updateFlipStatus(const std::vector<std::string>& strs, bool isFlip[4][15], char userAnswer) {
+		for(int y = 0; y < strs.size(); y++) {
+			for(int x = 0; x < strs[y].size(); x++) {
+				if(isFlip[y][x]) continue;
+				if(strs[y][x] != userAnswer) continue;
+				isFlip[y][x] = true;
+			}
+		}
+	}
+
+	int getPassCount(const std::vector<std::string>& strs, bool  isFlip[4][15], char userAnswer) {
+		int result = 0;
+		for(int y = 0; y < strs.size(); y++) {
+			for(int x = 0; x < strs[y].size(); x++) {
+				if(isFlip[y][x]) continue;
+				if(strs[y][x] != userAnswer) continue;
+				result++;
+			}
+		}
+		return result;
 	}
 };
