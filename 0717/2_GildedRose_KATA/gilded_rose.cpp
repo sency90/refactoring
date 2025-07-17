9befa23c7814d95f687f67348519bcba399612a7
Donghyuk.Yang <sency90@gmail.com>
2025-07-17 14:51:42 +0900

    step1. constanct refactoring

--- 0717/2_GildedRose_KATA/gilded_rose.cpp
+++ 0717/2_GildedRose_KATA/gilded_rose.cpp
@@ -3,15 +3,21 @@
 using std::vector;
 using std::string;
 
+namespace{
+	const char* SULFURAS = "Sulfuras, Hand of Ragnaros";
+	const char* AGED_BRIE = "Aged Brie";
+	const char* BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
+};
+
 GildedRose::GildedRose(vector<Item>& items) : items(items) {
 }
 
 void GildedRose::updateQuality() {
 	for(int i = 0; i < items.size(); i++) {
 		Item & item = items[i];
-		if(item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
+		if(item.name != AGED_BRIE && item.name != BACKSTAGE_PASS) {
 			if(item.quality > 0) {
-				if(item.name != "Sulfuras, Hand of Ragnaros") {
+				if(item.name != SULFURAS) {
 					item.quality = item.quality - 1;
 				}
 			}
@@ -20,7 +26,7 @@ void GildedRose::updateQuality() {
 			if(item.quality < 50) {
 				item.quality = item.quality + 1;
 
-				if(item.name == "Backstage passes to a TAFKAL80ETC concert") {
+				if(item.name == BACKSTAGE_PASS) {
 					if(item.sellIn < 11) {
 						if(item.quality < 50) {
 							item.quality = item.quality + 1;
@@ -36,15 +42,15 @@ void GildedRose::updateQuality() {
 			}
 		}
 
-		if(item.name != "Sulfuras, Hand of Ragnaros") {
+		if(item.name != SULFURAS) {
 			item.sellIn = item.sellIn - 1;
 		}
 
 		if(item.sellIn < 0) {
-			if(item.name != "Aged Brie") {
-				if(item.name != "Backstage passes to a TAFKAL80ETC concert") {
+			if(item.name != AGED_BRIE) {
+				if(item.name != BACKSTAGE_PASS) {
 					if(item.quality > 0) {
-						if(item.name != "Sulfuras, Hand of Ragnaros") {
+						if(item.name != SULFURAS) {
 							item.quality = item.quality - 1;
 						}
 					}
