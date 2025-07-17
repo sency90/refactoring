35f44acb2b11431f99122eda031f8e0b5fee2b19
Donghyuk.Yang <sency90@gmail.com>
2025-07-17 13:29:10 +0900

    Refactor: completed method-level cleanup; class-level refactoring will be covered in a separate example as per instructor

--- 0717/1_WhellOfFortune_KATA_TeacherCode/Wheel.cpp
+++ 0717/1_WhellOfFortune_KATA_TeacherCode/Wheel.cpp
@@ -1,120 +1,116 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <set>
 
 using std::string;
 using std::vector;
 
+struct Point {
+	int x, y;
+};
+
 class Wheel {
 public:
-	int getPrice(const vector<string> &strs, string userdata) {
-		bool isFlip[4][15] = {false};
+	int getPrice(const vector<string> &strs, const string &userdata) {
+		int totalPrice = 0;
 
-		int continueTouchedCount = 0;
-		bool isPossibleGetFirstChance[4] = {true, true, true, true};
-
-		int sum = 0;
-		int isOpenSecondChance[4] = {false};
-
-		//하나씩 처리
-		//26글자 for 돌면서 퀴즈 참석자가 하나씩 시도를 하는 것
-		for(int i = 0; i < 26; i++) {
-
+		for(int i = 0; i < MAX_TRY_ANSWER_COUNT; i++) {
 			char userAnswer = userdata[i];
 
-			//동일한 문자 개수를 Counting한다.
-			int passCnt = getPassCount(strs, isFlip, userAnswer);
+			vector<Point> answerPoints = getAnswerPoints(strs, userAnswer);
 
+			int passCnt = answerPoints.size();
 			if(passCnt == 0) {
 				continueTouchedCount = 0;
-				for(int i = 0; i < 4; i++) isOpenSecondChance[i] = false;
+				clearSecondChanceStatus();
 				continue;
 			}
 
-			vector<int> firstLines = getFirstChanceLines(strs, isFlip, userAnswer, isPossibleGetFirstChance);
-			int secondChanceCount = getSecondChanceCount(strs, isOpenSecondChance, isFlip, userAnswer);
+			continueTouchedCount += 1;
 
+			//First, Second 찬스 확인하기
+			vector<int> firstLines = getFirstChanceLines(answerPoints);
+			int secondChanceCount = getSecondChanceCount(answerPoints);
 
-			//상금얻자
-			sum += (++continueTouchedCount * 100) * passCnt;
-			sum += (1000 * firstLines.size());
-			sum += (secondChanceCount * 2000);
+			//상금 
+			totalPrice += (100 * continueTouchedCount) * passCnt;
+			totalPrice += (1000 * firstLines.size());
+			totalPrice += (2000 * secondChanceCount);
 
-			updateFirstChanceStatus(strs, isFlip, userAnswer, isPossibleGetFirstChance);
-			updateSecondChanceStatus(firstLines, isOpenSecondChance);
-			updateFlipStatus(strs, isFlip, userAnswer);
+			//게임 상태 업데이트
+			updateFirstChanceStatus(answerPoints);
+			updateSecondChanceStatus(firstLines);
+			updateFlipStatus(answerPoints);
 		}
 
-		return sum;
+		return totalPrice;
 	}
-	void updateSecondChanceStatus(std::vector<int>& firstLines, int isOpenSecondChance[4]) {
-		clearSecondChanceStatus(isOpenSecondChance);
+
+private:
+	vector<Point> getAnswerPoints(const std::vector<std::string>& strs, char userAnswer) const {
+		vector<Point> points;
+		int result = 0;
+		for(int y = 0; y < strs.size(); y++) {
+			for(int x = 0; x < strs[y].size(); x++) {
+				if(isFlip[y][x]) continue;
+				if(strs[y][x] != userAnswer) continue;
+				points.push_back({x, y});
+			}
+		}
+		return points;
+	}
+
+	std::vector<int> getFirstChanceLines(const vector<Point>& answerPoints) const {
+		std::vector<int> firstLines;
+		for(Point point : answerPoints) {
+			if(point.x != 0) continue;
+			if(isPossibleGetFirstChance[point.y] == false) continue;
+			firstLines.push_back(point.y);
+		}
+		return firstLines;
+	}
+
+	int getSecondChanceCount(vector<Point> answerPoints) const {
+		std::set<int> DistintLines;
+
+		for(Point point : answerPoints) {
+			if(isOpenSecondChance[point.y] == false) continue;
+			DistintLines.insert(point.y);
+		}
+
+		return DistintLines.size();
+	}
+
+	void updateFirstChanceStatus(const vector<Point>& answerPoints) {
+		for(Point point : answerPoints) {
+			isPossibleGetFirstChance[point.y] = false;
+		}
+	}
+
+	void updateSecondChanceStatus(const std::vector<int>& firstLines) {
+		clearSecondChanceStatus();
 		for(int line : firstLines) {
 			isOpenSecondChance[line] = true;
 		}
 	}
 
-	void updateFirstChanceStatus(const std::vector<std::string>& strs, bool  isFlip[4][15], char userAnswer, bool isPossibleGetFirstChance[4]) {
-		for(int y = 0; y < strs.size(); y++) {
-			for(int x = 0; x < strs[y].size(); x++) {
-				if(isFlip[y][x]) continue;
-				if(strs[y][x] != userAnswer) continue;
-				isPossibleGetFirstChance[y] = false;
-			}
+	void clearSecondChanceStatus() {
+		std::fill(isOpenSecondChance, isOpenSecondChance + ANSWER_LINE_COUNT, false);
+	}
+
+	void updateFlipStatus(const vector<Point>& answerPoints) {
+		for(Point point : answerPoints) {
+			isFlip[point.y][point.x] = true;
 		}
 	}
 
-	std::vector<int> getFirstChanceLines(const std::vector<std::string>& strs, bool  isFlip[4][15], char userAnswer, bool isPossibleGetFirstChance[4]) {
-		std::vector<int> firstLines;
-		for(int y = 0; y < strs.size(); y++) {
-			for(int x = 0; x < strs[y].size(); x++) {
-				if(isFlip[y][x]) continue;
-				if(strs[y][x] != userAnswer) continue;
-				if(isPossibleGetFirstChance[y] == false) continue;
-				if(x != 0) continue;
-				firstLines.push_back(y);
-			}
-		}
-		return firstLines;
-	}
+	static const int MAX_TRY_ANSWER_COUNT = 26;
+	static const int ANSWER_LINE_COUNT = 4;
+	static const int ANSWER_LENGTH_COUNT = 15;
 
-	void clearSecondChanceStatus(int isOpenSecondChance[]) {
-		std::fill(isOpenSecondChance, isOpenSecondChance + 4, false);
-	}
-
-	int getSecondChanceCount(const std::vector<std::string>& strs, int isOpenSecondChance[4], bool isFlip[4][15], char userAnswer) {
-		int result = 0;
-		for(int y = 0; y < strs.size(); y++) {
-			if(isOpenSecondChance[y] == true) {
-				for(int x = 0; x < strs[y].size(); x++) {
-					if(isFlip[y][x]) continue;
-					if(strs[y][x] != userAnswer) continue;
-					result += 1;
-					break;
-				}
-			}
-		}
-		return result;
-	}
-	void updateFlipStatus(const std::vector<std::string>& strs, bool isFlip[4][15], char userAnswer) {
-		for(int y = 0; y < strs.size(); y++) {
-			for(int x = 0; x < strs[y].size(); x++) {
-				if(isFlip[y][x]) continue;
-				if(strs[y][x] != userAnswer) continue;
-				isFlip[y][x] = true;
-			}
-		}
-	}
-
-	int getPassCount(const std::vector<std::string>& strs, bool  isFlip[4][15], char userAnswer) {
-		int result = 0;
-		for(int y = 0; y < strs.size(); y++) {
-			for(int x = 0; x < strs[y].size(); x++) {
-				if(isFlip[y][x]) continue;
-				if(strs[y][x] != userAnswer) continue;
-				result++;
-			}
-		}
-		return result;
-	}
+	bool isFlip[ANSWER_LINE_COUNT][ANSWER_LENGTH_COUNT] = {false};
+	bool isPossibleGetFirstChance[ANSWER_LINE_COUNT] = {true, true, true, true};
+	int isOpenSecondChance[ANSWER_LINE_COUNT] = {false};
+	int continueTouchedCount = 0;
 };
