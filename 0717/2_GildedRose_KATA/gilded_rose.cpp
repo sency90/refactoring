bd0f4532f060fdd644a150155108b75c54a816f5
Donghyuk.Yang <sency90@gmail.com>
2025-07-17 15:07:33 +0900

    step6. if-condition moving

--- 0717/2_GildedRose_KATA/gilded_rose.cpp
+++ 0717/2_GildedRose_KATA/gilded_rose.cpp
@@ -49,17 +49,21 @@ void GildedRose::updateQuality() {
 			item.sellIn = item.sellIn - 1;
 		}
 
-		if(item.sellIn < 0) {
-			if(item.name == AGED_BRIE) {
+		if(item.name == AGED_BRIE) {
+			if(item.sellIn < 0) {
 				if(item.quality < 50) {
 					item.quality = item.quality + 1;
 				}
 			}
-			else {
-				if(item.name == BACKSTAGE_PASS) {
+		}
+		else {
+			if(item.name == BACKSTAGE_PASS) {
+				if(item.sellIn < 0) {
 					item.quality = item.quality - item.quality;
 				}
-				else {
+			}
+			else {
+				if(item.sellIn < 0) {
 					if(item.quality > 0) {
 						if(item.name != SULFURAS) {
 							item.quality = item.quality - 1;
