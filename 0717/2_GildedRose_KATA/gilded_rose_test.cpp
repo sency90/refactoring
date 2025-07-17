ab54db89fe89f1ad9ce38755448cf2a890d05139
Donghyuk.Yang <sency90@gmail.com>
2025-07-17 13:55:03 +0900

    2_GildedRose_KATA 1st Commit

---
+++ 0717/2_GildedRose_KATA/gilded_rose_test.cpp
@@ -0,0 +1,20 @@
+#include "gmock/gmock.h"
+#include "gilded_rose.h"
+
+using std::vector;
+using std::string;
+
+TEST(GildedRoseTest, Foo) {
+	//Arrange
+	vector<Item> items;
+	//items.push_back(Item("Foo", 0, 0));
+	items.emplace_back("Foo", 0, 0);
+	GildedRose app(items);
+
+	//Act
+	app.updateQuality();
+
+	//Arrange
+	EXPECT_EQ("Foo", app.items[0].name);
+}
+
