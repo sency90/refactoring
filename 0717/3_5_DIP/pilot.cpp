d76f81637635f021aab7a56aac0e2a8473ec35e9
Donghyuk.Yang <sency90@gmail.com>
2025-07-17 16:56:19 +0900

    add some comments for DIP

--- 0717/3_5_DIP/pilot.cpp
+++ 0717/3_5_DIP/pilot.cpp
@@ -3,7 +3,11 @@
 class Pilot {
 public:
     Pilot() {
-        vehicle_ = new RacingCar(100);
+        //Pilot이 Racing Car와 같이 태어난다. 
+        //이러면 파일럿은 처음부터 어떤 차량에 종속될 수 밖에 없다. 
+        //다른 차량도 이용할 수 있도록 하려면 의존관계를 뒤집어야 한다. 
+        //-> DIP: Dependency Inversion Principle
+        vehicle_ = new RacingCar(100); 
     }
 
     void IncreaseSpeed() {
