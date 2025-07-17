275faeb738a25168fb416b512d686f74b0702e69
Donghyuk.Yang <sency90@gmail.com>
2025-07-17 14:19:30 +0900

    Add 3 Unit Test Cases

--- 0717/2_GildedRose_KATA/gilded_rose_test.cpp
+++ 0717/2_GildedRose_KATA/gilded_rose_test.cpp
@@ -18,3 +18,37 @@ TEST(GildedRoseTest, Foo){
 	EXPECT_EQ("Foo", app.items[0].name);
 }
 
+TEST(GildedRoseTest, NoNameSellin0Quality0) {
+	vector<Item> items;
+	items.emplace_back("noname", 0,0);
+	GildedRose app{ items };
+
+	app.updateQuality();
+
+	EXPECT_EQ(-1, items[0].sellIn);
+	EXPECT_EQ(0, items[0].quality);
+}
+
+TEST(GildedRoseTest, NoNameSellin0Quality1) {
+	vector<Item> items;
+	items.emplace_back("noname", 0,1);
+	GildedRose app{ items };
+
+	app.updateQuality();
+
+	EXPECT_EQ(-1, items[0].sellIn);
+	EXPECT_EQ(0, items[0].quality);
+}
+
+
+TEST(GildedRoseTest, SulfurasSellin0Quality80) {
+	vector<Item> items;
+	items.emplace_back("Sulfuras, Hand of Ragnaros", 0,80);
+	GildedRose app{ items };
+
+	app.updateQuality();
+
+	EXPECT_EQ(0, items[0].sellIn);
+	EXPECT_EQ(80, items[0].quality);
+}
+
