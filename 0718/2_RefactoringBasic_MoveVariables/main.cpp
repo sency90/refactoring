3d26a33e0c6206df41d30bab4b048ebe77a50708
Donghyuk.Yang <sency90@gmail.com>
2025-07-18 10:08:40 +0900

    solved 0718_2 example problem

--- 0718/2_RefactoringBasic_MoveVariables/main.cpp
+++ 0718/2_RefactoringBasic_MoveVariables/main.cpp
@@ -2,27 +2,50 @@
 #include "gmock/gmock.h"
 
 class Player {
+private:
+    std::string name = "hwan";
+    int level_ = 10;
+    int hp_ = 180;
+    int mp_ = 200;
+
+public:
+    Player(const std::string & name, int level, int hp, int mp):name(name), level_(level), hp_(hp), mp_(mp) {}
+
+    void AddLevel(int level) {
+        level_ += level;
+    }
+    void AddHp(int hp) {
+        hp_ += hp;
+    }
+    void AddMp(int mp) {
+        mp_ += mp;
+    }
+
+    int GetHp() const {
+        return hp_;
+    }
+    int GetMp() const {
+        return mp_;
+    }
+    int GetLevel() const {
+        return level_;
+    }
 };
 
 
 TEST(Game, GamePlayerEffect) {
-    int level = 10;
-    int hp = 180;
-    int mp = 200;
-    std::string name = "hwan";
-
+    Player player("hwan", 10, 180, 200);
     // level up effect 
-    hp += 10;
-    mp -= 20;
-    level += 1; 
+	player.AddLevel(1);
+	player.AddHp(10);
+	player.AddMp(-20);
 
-    EXPECT_EQ(190, hp); 
-    EXPECT_EQ(180, mp); 
-    EXPECT_EQ(11, level); 
+    EXPECT_EQ(11, player.GetLevel()); 
+    EXPECT_EQ(190, player.GetHp()); 
+    EXPECT_EQ(180, player.GetMp()); 
 }
 
 int main() {
     testing::InitGoogleTest();
-
     return RUN_ALL_TESTS();
 }
