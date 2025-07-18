a586cb9791c4eec0f0b5606c55e16cdf66d7ecf7
Donghyuk.Yang <sency90@gmail.com>
2025-07-18 16:04:15 +0900

    for문 나눔 - amount 계산부분과 frequency 계산 부분을 나눔

--- 0718/5_VideoRental_KATA/customer.cpp
+++ 0718/5_VideoRental_KATA/customer.cpp
@@ -39,30 +39,34 @@ public:
 			//determine amounts for rental
 			switch(each.getMovie().getPriceCode()) {
 			case Movie::REGULAR:
-			thisAmount += 2;
-			if(each.getDaysRented() > 2)
-				thisAmount += (each.getDaysRented() - 2) * 1.5;
-			break;
+				thisAmount += 2;
+				if(each.getDaysRented() > 2) {
+					thisAmount += (each.getDaysRented() - 2) * 1.5;
+				}
+				break;
 			case Movie::NEW_RELEASE:
-			thisAmount += each.getDaysRented() * 3;
-			break;
+				thisAmount += each.getDaysRented() * 3;
+				break;
 			case Movie::CHILDREN:
-			thisAmount += 1.5;
-			if(each.getDaysRented() > 3)
-				thisAmount += (each.getDaysRented() - 3) * 1.5;
-			break;
+				thisAmount += 1.5;
+				if(each.getDaysRented() > 3) {
+					thisAmount += (each.getDaysRented() - 3) * 1.5;
+				}
+				break;
 			}
 
+			// show figures for this rental
+			result += "\t" + each.getMovie().getTitle() + "\t" + roundDoubleToString(thisAmount) + "\n";
+			totalAmount += thisAmount;
+		}
+
+		for(Rental each : rentals) {
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two day new release rental
 			if(each.getMovie().getPriceCode() == Movie::NEW_RELEASE && each.getDaysRented() > 1) {
 				frequentRenterPoints++;
 			}
-
-			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t" + roundDoubleToString(thisAmount) + "\n";
-			totalAmount += thisAmount;
 		}
 
 		// add footer lines
