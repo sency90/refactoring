2c5c3e40e20ce5a9c4275361a6c752147979b5d7
Donghyuk.Yang <sency90@gmail.com>
2025-07-18 17:02:33 +0900

    5_VideoRental_KATA Chapter2. 내 나름대로 Method Level Refactoring까지 진행함. (강사 코드와 비교 필요)

--- 0718/5_VideoRental_KATA/customer.cpp
+++ 0718/5_VideoRental_KATA/customer.cpp
@@ -11,11 +11,6 @@ private:
 	string name;
 	vector<Rental> rentals;
 
-	string roundDoubleToString(double tar) {
-		char buf[20] = {0};
-		sprintf_s(buf, sizeof(buf), "%.1f", tar);
-		return string{buf};
-	}
 
 public:
 	Customer(const string& name) : name{name} {}
@@ -29,50 +24,88 @@ public:
 	}
 
 	string statement() {
+		string result = makeHeaderForStatement();
+
 		double totalAmount = 0;
-		int frequentRenterPoints = 0;
-		string result = "Rental Record for " + getName() + "\n";
-
-		for(Rental each : rentals) {
-			double thisAmount = 0;
-
+		for(Rental rental : rentals) {
 			//determine amounts for rental
-			switch(each.getMovie().getPriceCode()) {
-			case Movie::REGULAR:
-				thisAmount += 2;
-				if(each.getDaysRented() > 2) {
-					thisAmount += (each.getDaysRented() - 2) * 1.5;
-				}
-				break;
-			case Movie::NEW_RELEASE:
-				thisAmount += each.getDaysRented() * 3;
-				break;
-			case Movie::CHILDREN:
-				thisAmount += 1.5;
-				if(each.getDaysRented() > 3) {
-					thisAmount += (each.getDaysRented() - 3) * 1.5;
-				}
-				break;
-			}
+			double thisAmount = getCharge(rental);
+			totalAmount += thisAmount;
 
 			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t" + roundDoubleToString(thisAmount) + "\n";
-			totalAmount += thisAmount;
+			result += makeBodyForStatement(rental, thisAmount);
 		}
 
-		for(Rental each : rentals) {
-			// add frequent renter points
-			frequentRenterPoints++;
-			// add bonus for a two day new release rental
-			if(each.getMovie().getPriceCode() == Movie::NEW_RELEASE && each.getDaysRented() > 1) {
-				frequentRenterPoints++;
-			}
-		}
+		int frequentRenterPoints = getFrequentRentalPoints();
 
 		// add footer lines
-		result += "Amount owed is " + roundDoubleToString(totalAmount) + "\n";
-		result += "You earned " + std::to_string(frequentRenterPoints) + " frequent renter points";
+		result += makeFooterForStatement(totalAmount, frequentRenterPoints);
 
 		return result;
+	}
+
+	std::string makeHeaderForStatement() {
+		 return std::string("Rental Record for ") + getName() + "\n";
+	}
+
+	std::string makeBodyForStatement(Rental &rental, double thisAmount) {
+		return std::string("\t") + rental.getMovie().getTitle() + "\t" + roundDoubleToString(thisAmount) + "\n";
+	}
+
+	std::string makeFooterForStatement(double totalAmount, int frequentRenterPoints) {
+		std::string result;
+		result += "Amount owed is " + roundDoubleToString(totalAmount) + "\n";
+		result += "You earned " + std::to_string(frequentRenterPoints) + " frequent renter points";
+		return result;
+	}
+
+	int getFrequentRentalPoints() {
+		int result=0;
+		for(Rental rental : rentals) {
+			// add bonus for a two day new release rental
+			if(rental.getMovie().getPriceCode() == Movie::NEW_RELEASE && rental.getDaysRented() > 1) {
+				result+=2;
+			}
+			else { //normal case
+				result++; // add frequent renter points
+			}
+		}
+		return result;
+	}
+
+	double getCharge(Rental &rental) {
+		switch(rental.getMovie().getPriceCode()) {
+		case Movie::REGULAR:
+			return getRegularCharge(rental);
+		case Movie::NEW_RELEASE:
+			return getNewReleaseCharge(rental);
+		case Movie::CHILDREN:
+			return getChildrenCharge(rental);
+		default:
+			return 0.0;
+		}
+	}
+	double getChildrenCharge(Rental & rental) {
+		double result = 1.5;
+		if(rental.getDaysRented() > 3) {
+			result += (rental.getDaysRented() - 3) * 1.5;
+		}
+		return result;
+	}
+	double getNewReleaseCharge(Rental & rental) {
+		return rental.getDaysRented() * 3.0;
+	}
+	double getRegularCharge(Rental & rental) {
+		double result = 2;
+		if(rental.getDaysRented() > 2) {
+			result += (rental.getDaysRented() - 2) * 1.5;
+		}
+		return result;
+	}
+
+	string roundDoubleToString(double tar) {
+		char buf[20] = {0};
+		sprintf_s(buf, sizeof(buf), "%.1f", tar);
+		return string{buf};
 	}
 };
