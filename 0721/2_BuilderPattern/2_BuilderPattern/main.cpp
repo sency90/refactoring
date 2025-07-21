2180f93456869ab6fc3f0dee7488465d2a3afcef
Donghyuk.Yang <sency90@gmail.com>
2025-07-21 10:37:01 +0900

    2_BuilderPattern - Joshua Bloch의 Builder Pattern을 만듦.

--- 0721/2_BuilderPattern/2_BuilderPattern/main.cpp
+++ 0721/2_BuilderPattern/2_BuilderPattern/main.cpp
@@ -1,7 +1,10 @@
 #include <string>
 #include <cstdio>
 #define interface struct
-#define METHOD_CHAINING_METHOD
+//#define CONSTRUCTOR_METHOD
+//#define SETTER_METHOD
+//#define METHOD_CHAINING_METHOD
+#define JOSHUA_BLOCH_BUILDER_PATTERN
 
 class Cut {
 public:
@@ -42,8 +45,43 @@ public:
 		back = b;
 		return *this;
 	}
+#elif defined JOSHUA_BLOCH_BUILDER_PATTERN
+public:
+	Cut(const std::string & style_name, int front, int side, int back)
+		: style_name(style_name), front(front), side(side), back(back) {}
+		
+	class HairStyle {
+	public:
+		HairStyle& setStyleName(const std::string& style_name) {
+			this->style_name = style_name;
+			return *this;
+		}
+		HairStyle& setFront(int f) {
+			front = f;
+			return *this;
+		}
+		HairStyle& setSide(int s) {
+			side = s;
+			return *this;
+		}
+		HairStyle& setBack(int b) {
+			back = b;
+			return *this;
+		}
+
+		Cut* build() {
+			if(style_name.empty() || front==0 || back==0 || side==0) {
+				throw std::exception("ERROR!!");
+			}
+			return new Cut(style_name, front, back, side);
+		}
+	private:
+		std::string style_name;
+		int front=0, back=0, side=0;
+	};
 #endif
 
+public:
 	void print() const {
 		printf("style_name: %s\n", style_name.c_str());
 		printf("front: %d, back:%d, side:%d\n", front, back, side);
@@ -57,15 +95,21 @@ private:
 int main() {
 #ifdef CONSTRUCTOR_METHOD
 	Cut cut("good-style", 3,3,2);
+	cut.print();
 #elif defined SETTER_METHOD
 	Cut cut;
 	cut.setStyleName("good-style");
 	cut.setBack(3);
 	cut.setSide(3);
 	cut.setFront(2);
+	cut.print();
 #elif defined METHOD_CHAINING_METHOD
+
 	Cut & cut = (*(new Cut())).setStyleName("good-style").setFront(2).setBack(3).setSide(3);
 	cut.print();
+#elif defined JOSHUA_BLOCH_BUILDER_PATTERN
+	Cut *cut = Cut::HairStyle().setStyleName("good-style").setFront(2).setBack(3).setSide(3).build();
+	cut->print();
 #endif
 	return 0;
 }
