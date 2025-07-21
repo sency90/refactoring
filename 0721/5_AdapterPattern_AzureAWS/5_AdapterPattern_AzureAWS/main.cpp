ceab107226eebac2561d329e35219228017af80a
Donghyuk.Yang <sency90@gmail.com>
2025-07-21 11:06:03 +0900

    5_AdapterPattern_AzureAWS Complete

--- 0721/5_AdapterPattern_AzureAWS/5_AdapterPattern_AzureAWS/main.cpp
+++ 0721/5_AdapterPattern_AzureAWS/5_AdapterPattern_AzureAWS/main.cpp
@@ -50,10 +50,40 @@ public:
     }
 };
 
+class Adapter : public Azure {
+public:
+    virtual void ms_connect() {
+        //std::cout << "[Azure] Connected\n";
+    }
+    virtual void ms_login(const std::string& id, const std::string& pass) {
+        //std::cout << "[Azure] Login : " << id << " pass : " << pass << "\n";
+        aws.aws_conn(id, pass);
+    }
+    virtual void ms_sendData() {
+        //std::cout << "[Azure] Data sent\n";
+        aws.aws_setData();
+    }
+    virtual void ms_receiveData() {
+        //std::cout << "[Azure] Data received\n";
+        aws.aws_getData();
+    }
+    virtual void ms_disconnect() {
+        //std::cout << "[Azure] Disconnected\n";
+        aws.aws_bye();
+    }
+private:
+    AWS aws;
+};
+
 //Client Code
 int main() {
     ImportantModule im;
 
+#if 0
     Azure azure;
     im.run(azure);
+#else
+    Adapter adapter;
+    im.run(adapter);
+#endif
 }
