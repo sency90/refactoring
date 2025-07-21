fe896ad169b42e313a00c816f42d263adcf4e45f
plzrun <sency90@gmail.com>
2025-07-22 02:38:52 +0900

    3_Singleton: 이렇게 변경하면, 객체 생성이 thread-safe함. (단, 객체 사용시에는 mutex 필요)

--- 0721/3_Singleton/main.cpp
+++ 0721/3_Singleton/main.cpp
@@ -3,13 +3,10 @@
 #define interface struct
 
 
-//객체가 무조건 1개만 생성할수있도록 강제
 class Single {
 public:
-    static Single *getInstance() {
-        if (instance == nullptr) {
-            instance = new Single();
-        }
+    static Single &getInstance() {
+        static Single instance;
         return instance;
     }
 
@@ -17,22 +14,20 @@ public:
         std::cout << "RUN\n";
     }
 
-private:
-    static Single* instance;
+private:    
     Single() {}
 };
 
-Single* Single::instance = nullptr;
-
 int main() {
+    Single t = Single::getInstance();
+    t.run();
+
     //Single t;
     //Single* t = new Single();
 
-    Single* t1 = Single::getInstance();
-    t1->run();
+    //Single* t1 = Single::getInstance();
+    //t1->run();
 
-    Single* t2 = Single::getInstance();
-    t2->run();
-
-    return 0;
+    //Single* t2 = Single::getInstance();
+    //t2->run();
 }
