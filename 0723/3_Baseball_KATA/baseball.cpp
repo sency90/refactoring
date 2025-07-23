68bbb0ea710b70589bff3c41fccfa608af568cd8
Donghyuk.Yang <sency90@gmail.com>
2025-07-23 12:51:17 +0900

    [faeture] page 27

---
+++ 0723/3_Baseball_KATA/baseball.cpp
@@ -0,0 +1,8 @@
+#include <stdexcept>
+using namespace std;
+class Baseball{
+public:
+	void guess(const string & s) {
+		throw length_error("Must be three letters.");
+	}
+};
--- 0723/3_Baseball_KATA/baseball_test.cpp
+++ 0723/3_Baseball_KATA/baseball_test.cpp
@@ -4,3 +4,8 @@
 TEST(TS, TC1) {
 	EXPECT_EQ(1,1);
 }
+
+TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmached) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12")), length_error);
+}
