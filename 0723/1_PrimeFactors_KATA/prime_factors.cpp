af15634e592d9e6712aa8cfa2cae5eaa0e3fa0fe
Donghyuk.Yang <sency90@gmail.com>
2025-07-23 10:01:29 +0900

    [feature] page 56

--- 0723/1_PrimeFactors_KATA/prime_factors.cpp
+++ 0723/1_PrimeFactors_KATA/prime_factors.cpp
@@ -11,7 +11,12 @@ public:
 					result.push_back(2);
 					number /= 2;
 				}
-			} else {
+			} 
+			else if(number == 6) {
+				result.push_back(2);
+				result.push_back(3);
+			}
+			else {
 				result.push_back(number);
 			}
 		}
