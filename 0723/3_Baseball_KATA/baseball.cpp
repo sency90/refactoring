72254e2a1c3ef5bcb8fe76cb3f1c1725f3d2c07e
Donghyuk.Yang <sency90@gmail.com>
2025-07-23 12:54:48 +0900

    [feature] page 30

--- 0723/3_Baseball_KATA/baseball.cpp
+++ 0723/3_Baseball_KATA/baseball.cpp
@@ -1,10 +1,18 @@
 #include <stdexcept>
+#include <string>
 using namespace std;
+
 class Baseball {
 public:
 	void guess(const string& s) {
 		if(s.length() != 3) {
 			throw length_error("Must be three letters.");
+		}
+
+		for(char ch : s) {
+			if(false == isdigit(ch)) {
+				throw invalid_argument("Must be number");
+			}
 		}
 	}
 };
