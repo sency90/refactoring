cb29265f963e08b5f788c226053952b5c0602ce7
Donghyuk.Yang <sency90@gmail.com>
2025-07-23 09:54:51 +0900

    [refactoring] page 46

--- 0723/1_PrimeFactors_KATA/prime_factors_test.cpp
+++ 0723/1_PrimeFactors_KATA/prime_factors_test.cpp
@@ -2,17 +2,21 @@
 #include "prime_factors.cpp"
 #include <vector>
 using std::vector;
+using namespace testing;
 
-TEST(PrimeFactors, Of1) {
+class PrimeFixture : public Test{
+public:
 	PrimeFactors prime_factor;
-	vector<int> expected = {};
+	vector<int> expected;
+};
+
+TEST_F(PrimeFixture, Of1) {
+	expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
-	EXPECT_EQ(1,1);
 }
 
-TEST(PrimeFactors, Of2) {
-	PrimeFactors prime_factor;
-	vector<int> expected = {2};
+TEST_F(PrimeFixture, Of2) {
+	expected = {2};
 	EXPECT_EQ(expected, prime_factor.of(2));
 }
 
