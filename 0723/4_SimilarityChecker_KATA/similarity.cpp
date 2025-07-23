881e09dc390499bb4e935fc296002239ef12e755
Donghyuk.Yang <sency90@gmail.com>
2025-07-23 15:46:25 +0900

    [refactoring] add part score logic

--- 0723/4_SimilarityChecker_KATA/similarity.cpp
+++ 0723/4_SimilarityChecker_KATA/similarity.cpp
@@ -3,21 +3,20 @@
 #include <algorithm>
 
 namespace {
-	constexpr int LENGTH_PERFECT_SCORE = 60;
-	constexpr int ZERO_SCORE = 0;
-	constexpr int UNREACHABLE_CODE = -1;
-	
+	constexpr double LENGTH_PERFECT_SCORE = 60.0;
+	constexpr double ZERO_SCORE = 0.0;
 }
 
 class Similarity {
 public:
-	int getLengthScore(const std::string & str1, const std::string & str2) {
+	double getLengthScore(const std::string & str1, const std::string & str2) {
 		if(str1.size() == str2.size()) return LENGTH_PERFECT_SCORE;
-		int len1 = str1.size(), len2 = str2.size();
+		int len1 = str1.size();
+		int len2 = str2.size();
 		int bigLen = std::max(len1,len2);
 		int smallLen = std::min(len1,len2);
 		if(bigLen >= 2*smallLen) return ZERO_SCORE;
 
-		return smallLen*60/bigLen;
+		return static_cast<double>(smallLen)*60/bigLen;
 	}
 };
--- 0723/4_SimilarityChecker_KATA/similarity_test.cpp
+++ 0723/4_SimilarityChecker_KATA/similarity_test.cpp
@@ -6,24 +6,28 @@ using namespace testing;
 class SimilarityFixture: public Test{
 public:
 	Similarity similarity;
-	void validCheck(int expectedLenthScore, const std::string & str1, const std::string & str2) {
+	void validCheck(double expectedLenthScore, const std::string & str1, const std::string & str2) {
 		EXPECT_EQ(expectedLenthScore, similarity.getLengthScore(str1, str2));
 	}
 };
 
 TEST_F(SimilarityFixture, LengthSame) {
-	validCheck(60, "ABC", "GHI");
+	validCheck(60.0, "ABC", "GHI");
 }
 
 TEST_F(SimilarityFixture, LengthX2) {
-	validCheck(0, "ABC", "GHIDEF");
+	validCheck(0.0, "ABC", "GHIDEF");
 }
 
 TEST_F(SimilarityFixture, LengthX2Over) {
-	validCheck(0, "ABC", "GHIDEFABC");
+	validCheck(0.0, "ABC", "GHIDEFABC");
 }
 
-TEST_F(SimilarityFixture, LengthPartScoreTotallyDifferent) {
-	validCheck(36, "ABC", "DEFGH");
+TEST_F(SimilarityFixture, LengthPartScore1) {
+	validCheck(36.0, "ABC", "DEFGH");
+}
+
+TEST_F(SimilarityFixture, LengthPartScore2) {
+	validCheck(7.0*60/9, "ABCDEFG", "ABCDEFGHI");
 }
 
