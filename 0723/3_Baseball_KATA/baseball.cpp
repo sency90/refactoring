3d637bdd81c2a47e8b64401630e2331fceed53de
Donghyuk.Yang <sency90@gmail.com>
2025-07-23 13:10:40 +0900

    [feature] page 49

--- 0723/3_Baseball_KATA/baseball.cpp
+++ 0723/3_Baseball_KATA/baseball.cpp
@@ -3,10 +3,20 @@
 #include <cctype>
 using namespace std;
 
+struct GuessResult {
+	bool solved;
+	int strikes;
+	int balls;
+};
+
+
 class Baseball {
 public:
-	void guess(const string& guessNumber) {
+	explicit Baseball(const string& question) : question(question) { }
+
+	GuessResult guess(const string& guessNumber) {
 		assertIllegalArgument(guessNumber);
+		return {true, 3, 0};
 	}
 	void assertIllegalArgument(const std::string& guessNumber) {
 		if(guessNumber.length() != 3) {
@@ -27,4 +37,6 @@ public:
 			|| guessNumber[0] == guessNumber[2]
 			|| guessNumber[1] == guessNumber[2];
 	}
+private:
+	string question;
 };
--- 0723/3_Baseball_KATA/baseball_test.cpp
+++ 0723/3_Baseball_KATA/baseball_test.cpp
@@ -4,7 +4,7 @@ using namespace testing;
 
 class BaseballFixture: public Test {
 public:
-	Baseball game;
+	Baseball game{"123"};
 	void assertIllegalArgument(const string& guessNumber) {
 		try {
 			game.guess(string(guessNumber));
@@ -19,4 +19,12 @@ TEST_F(BaseballFixture, ThrowExceptionWhenInvalidCase) {
 	assertIllegalArgument("12");
 	assertIllegalArgument("12s");
 	assertIllegalArgument("121");
+}
+
+TEST_F(BaseballFixture, ReturnSolvedResultIfMatchedNumber) {
+	GuessResult result = game.guess("123");
+
+	EXPECT_TRUE(result.solved);
+	EXPECT_EQ(3, result.strikes);
+	EXPECT_EQ(0, result.balls);
 }
