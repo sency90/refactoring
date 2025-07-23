971f01cc758285f89731540df281ac0f2481fdd3
Donghyuk.Yang <sency90@gmail.com>
2025-07-23 16:46:45 +0900

    [feature] 2.AplhaScore Feature - check same strings

--- 0723/4_SimilarityChecker_KATA/similarity_test.cpp
+++ 0723/4_SimilarityChecker_KATA/similarity_test.cpp
@@ -6,28 +6,35 @@ using namespace testing;
 class SimilarityFixture: public Test{
 public:
 	Similarity similarity;
-	void validCheck(double expectedLengthScore, const std::string & str1, const std::string & str2) {
+	void validLengthCheck(double expectedLengthScore, const std::string & str1, const std::string & str2) {
 		EXPECT_EQ(expectedLengthScore, similarity.getLengthScore(str1, str2));
+	}
+	void validAplhaCheck(double expectedAlphaScore, const std::string & str1, const std::string & str2) {
+		EXPECT_EQ(expectedAlphaScore, similarity.getAplhaScore(str1, str2));
 	}
 };
 
 TEST_F(SimilarityFixture, LengthSame) {
-	validCheck(60.0, "ABC", "GHI");
+	validLengthCheck(60.0, "ABC", "GHI");
 }
 
 TEST_F(SimilarityFixture, LengthX2) {
-	validCheck(0.0, "ABC", "GHIDEF");
+	validLengthCheck(0.0, "ABC", "GHIDEF");
 }
 
 TEST_F(SimilarityFixture, LengthX2Over) {
-	validCheck(0.0, "ABC", "GHIDEFABC");
+	validLengthCheck(0.0, "ABC", "GHIDEFABC");
 }
 
 TEST_F(SimilarityFixture, LengthPartScore1) {
-	validCheck(36.0, "ABC", "DEFGH");
+	validLengthCheck(36.0, "ABC", "DEFGH");
 }
 
 TEST_F(SimilarityFixture, LengthPartScore2) {
-	validCheck(7.0*60/9, "ABCDEFG", "ABCDEFGHI");
+	validLengthCheck(7.0*60/9, "ABCDEFG", "ABCDEFGHI");
 }
 
+
+TEST_F(SimilarityFixture, AlphaSame) {
+	validAplhaCheck(40.0, "ABC", "ABC");
+}
