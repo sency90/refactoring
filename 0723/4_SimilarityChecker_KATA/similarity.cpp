926bede058e9ea912e72b0029e2b432337ad930f
Donghyuk.Yang <sency90@gmail.com>
2025-07-23 15:26:16 +0900

    [refactoring] 1.LengthScore Feature - check same length test

--- 0723/4_SimilarityChecker_KATA/similarity.cpp
+++ 0723/4_SimilarityChecker_KATA/similarity.cpp
@@ -1,9 +1,15 @@
 #include <string>
 #include <cstring>
+#include <algorithm>
+
+namespace {
+	constexpr int LENGTH_PERFECT_SCORE = 60;
+	
+}
 
 class Similarity {
 public:
 	int getLengthScore(const std::string & str1, const std::string & str2) {
-		if(str1.size() == str2.size()) return 60;
+		if(str1.size() == str2.size()) return LENGTH_PERFECT_SCORE;
 	}
 };
