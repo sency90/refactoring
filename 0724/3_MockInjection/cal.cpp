2ebd5bc2882cea6289f0f9fb6a8cc3a033dc7ad3
Donghyuk.Yang <sency90@gmail.com>
2025-07-24 10:51:14 +0900

    3_MockInjection: Complete Practice

--- 0724/3_MockInjection/cal.cpp
+++ 0724/3_MockInjection/cal.cpp
@@ -1,4 +1,8 @@
-class Bank{
+class BankInterface {
+public:
+	virtual bool verify() = 0;
+};
+class Bank : public BankInterface{
 public:
 	bool verify() {
 		//로그인이 잘 되면... true
@@ -6,15 +10,17 @@ public:
 	}
 };
 
-class Cal{ //SUT
+class Cal{ //SUT(= System Under Test: Test 대상이 되는 Class나 Module을 말함)
 public:
+	Cal(BankInterface *bank) : bank(bank) {}
 	int getSum(int a, int b) {
-		Bank bank;
-
-		if(bank.verify()) {
+		if(bank->verify()) {
 			return a+b;
 		}
 
 		return -1;
 	}
+
+private:
+	BankInterface *bank;
 };
