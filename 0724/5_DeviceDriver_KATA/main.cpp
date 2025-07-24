50c3bc7e2b8f6a1fd0e5d7e8b409616ad6b18015
Donghyuk.Yang <sency90@gmail.com>
2025-07-24 17:30:08 +0900

    [refactoring] Add ReadBeforeWrite TestCase & Add ReturnsSameValueAfterFiveReads TestCase

--- 0724/5_DeviceDriver_KATA/main.cpp
+++ 0724/5_DeviceDriver_KATA/main.cpp
@@ -10,62 +10,54 @@ public:
 	MOCK_METHOD(void, write, (long addres, unsigned char data), (override));
 };
 
-TEST(DeviceDriver, ReadFromHW) {
-	// TODO : replace hardware with a Test Double
-	FlashMemoryDevice* hardware = new FlashDevice();
-	DeviceDriver driver{ hardware };
-	int data = driver.read(0xBU);
-	EXPECT_EQ(0xFF, data);
-}
+class DeviceDriverFixture: public Test{
+protected:
+	virtual void SetUp() override {
+		driver = new DeviceDriver{&mock};
+	}
 
-TEST(DeviceDriver, Read5Times) {
+public:
+	void expectWriteAfterEmptyCheck(int addr, int data) {
+		EXPECT_CALL(mock, read(addr))
+			.Times(1).WillOnce(Return(NO_DATA));
+
+		EXPECT_CALL(mock, write(addr, data))
+			.Times(1);
+
+		driver->write(addr, data);
+	}
+
+public:
 	FlashMock mock;
-	EXPECT_CALL(mock, read(0xBU)).Times(5);
+	DeviceDriver *driver;
+
+	const long MEMORY_ADDRESS = 0xBU;
+	const int NO_DATA = 0xFF;
+	const int INPUT_DATA = 10;
+
+};
+
+TEST_F(DeviceDriverFixture, Read5Times) {
+	EXPECT_CALL(mock, read(MEMORY_ADDRESS))
+		.Times(5);
 
 	//DeviceDriver가 read()하면 FlashMemoryDevice를 5번 read()해야 한다.
-	DeviceDriver driver{ &mock };
-	int data = driver.read(0xBU);
+	int data = driver->read(MEMORY_ADDRESS);
 }
 
-TEST(DeviceDriver, ReadBeforeWrite) {
-	//NiceMock은 stub이 없더라도 동작하게 해준다.
-	//stub: 더미인데 실제로 동작하는 것처럼 만든 것.
-	FlashMock mock;
-
-	EXPECT_CALL(mock, read(0xBU))
-		.Times(1).WillOnce(Return(0xFF));
-
-	EXPECT_CALL(mock, write(0xBU,_))
-		.Times(1);
-
-	DeviceDriver driver{&mock};
-	driver.write(0xBU, 7);
+TEST_F(DeviceDriverFixture, ReadBeforeWrite) {
+	expectWriteAfterEmptyCheck(MEMORY_ADDRESS, INPUT_DATA);
 }
 
-TEST(DeviceDriver, ReturnsSameValueAfterFiveReads) {
-	FlashMock mock;
-
-	long address = 0xBU;
-	int writtenData = 10;
-	int noData = 0xFF;
-
-#if 1 //write
-	EXPECT_CALL(mock, read(address))
-		.Times(1).WillOnce(Return(noData));
-
-	EXPECT_CALL(mock, write(address,writtenData))
-		.Times(1);
-
-	DeviceDriver driver{&mock};
-	driver.write(address, writtenData);
-#endif
+TEST_F(DeviceDriverFixture, ReturnsSameValueAfterFiveReads) {
+	expectWriteAfterEmptyCheck(MEMORY_ADDRESS, INPUT_DATA);
 
 #if 1 //read
-	EXPECT_CALL(mock, read(address))
-		.Times(5).WillRepeatedly(Return(writtenData));
+	EXPECT_CALL(mock, read(MEMORY_ADDRESS))
+		.Times(5).WillRepeatedly(Return(INPUT_DATA));
 
-	int outputData = driver.read(address);
-	EXPECT_EQ(outputData, writtenData);
+	int outputData = driver->read(MEMORY_ADDRESS);
+	EXPECT_EQ(outputData, INPUT_DATA);
 #endif
 }
 
