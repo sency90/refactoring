abde6c807545bf8fc03a89b739f8a105dcfcf935
Donghyuk.Yang <sency90@gmail.com>
2025-08-25 15:05:04 +0900

    [Change] using method chaining but it's still not enough

--- 0825/BuilderPattern/main.cpp
+++ 0825/BuilderPattern/main.cpp
@@ -3,28 +3,34 @@
 using namespace std;
 class Cut {
 public:
-	void setStyle(const std::string &style) {
+	Cut &setStyle(const std::string &style) {
 		this->style = style;
+		return *this;
 	}
 
-	void setFront(int front) {
+	Cut &setFront(int front) {
 		this->front = front;
+		return *this;
 	}
 
-	void setBack(int back) {
+	Cut &setBack(int back) {
 		this->back = back;
+		return *this;
 	}
 
-	void setSide(int side) {
+	Cut &setSide(int side) {
 		this->side = side;
+		return *this;
 	}
 
-	void setGuretnaru(int guretnaru) {
+	Cut &setGuretnaru(int guretnaru) {
 		this->guretnaru = guretnaru;
+		return *this;
 	}
 
-	void setMustache(int mustache) {
+	Cut &setMustache(int mustache) {
 		this->mustache = mustache;
+		return *this;
 	}
 
 private:
@@ -32,14 +38,10 @@ private:
 	int front, back, side, guretnaru, mustache;
 };
 int main() {
-	//그래서 setter를 이용하게 되면, 더럽다.
-	//가독성도 떨어지고, 전체적인 setting을 여러번 나눠서 해야하기 떄문에
-	//코드가 여기저기 흩어질 수도 있고
-	//setting 순서가 중요한 경우에도 이를 실수할 수 있다.
+	//그래서 method chaining 방식을 이용
+	//하지만, user(client)가 실수로 세팅 하나를 놓칠 수 있다.
+	//또한, setting순서가 중요한 경우에도 이를 실수할 수 있다.
 	Cut soldier_cut;
-	soldier_cut.setStyle("Soldier");
-	soldier_cut.setFront(1);
-	soldier_cut.setBack(1);
-	soldier_cut.setSide(1);
+	soldier_cut.setStyle("Soldier").setFront(1).setBack(1).setSide(1);
 	return 0;
 }
