7b609364482fb66b7fb805144308b41f9f19bb5a
Donghyuk.Yang <sency90@gmail.com>
2025-08-25 16:14:52 +0900

    [Change] using Builder Pattern but I think it's wrong...

--- 0825/BuilderPattern/main.cpp
+++ 0825/BuilderPattern/main.cpp
@@ -1,39 +1,79 @@
 ﻿#include <cstdio>
 #include <string>
+#include <stdexcept>
+#include <cstdlib>
 using namespace std;
 class Cut {
 public:
-	Cut &setStyle(const std::string &style) {
-		this->style = style;
-		return *this;
-	}
+	class Builder {
+	public:
+		Builder &setStyle(const std::string &style) {
+			this->style = style;
+			return *this;
+		}
 
-	Cut &setFront(int front) {
-		this->front = front;
-		return *this;
-	}
+		Builder &setFront(int front) {
+			this->front = front;
+			return *this;
+		}
 
-	Cut &setBack(int back) {
-		this->back = back;
-		return *this;
-	}
+		Builder &setBack(int back) {
+			this->back = back;
+			return *this;
+		}
 
-	Cut &setSide(int side) {
-		this->side = side;
-		return *this;
-	}
+		Builder &setSide(int side) {
+			this->side = side;
+			return *this;
+		}
 
-	Cut &setGuretnaru(int guretnaru) {
-		this->guretnaru = guretnaru;
-		return *this;
-	}
+		Builder &setGuretnaru(int guretnaru) {
+			this->guretnaru = guretnaru;
+			return *this;
+		}
 
-	Cut &setMustache(int mustache) {
-		this->mustache = mustache;
-		return *this;
+		Builder &setMustache(int mustache) {
+			this->mustache = mustache;
+			return *this;
+		}
+
+		Cut build() {
+			return Cut(style, front, back, side);
+		}
+
+
+	private:
+		std::string style;
+		int front, back, side, guretnaru, mustache;
+	};
+
+public:
+	Cut()=default;
+	void setCut(const Cut &cut) {
+		*this = cut;
+		validateArguments();
 	}
 
 private:
+	Cut(const std::string &style, int front, int back, int side, int guretnaru=0, int mustache=0)
+		: style(style), front(front), back(back), side(side), guretnaru(guretnaru), mustache(mustache) {
+	}
+
+	void validateArguments() const {
+		if(!style.empty()) {
+			throw std::invalid_argument("Style name must be set.");
+		}
+		if(front<=0) {
+			throw std::invalid_argument("Front hair must be greater than 0cm.");
+		}
+		if(back<=0) {
+			throw std::invalid_argument("...");
+		}
+		if(side<=0) {
+			throw std::invalid_argument("...");
+		}
+	}
+
 	std::string style;
 	int front, back, side, guretnaru, mustache;
 };
@@ -42,6 +82,6 @@ int main() {
 	//하지만, user(client)가 실수로 세팅 하나를 놓칠 수 있다.
 	//또한, setting순서가 중요한 경우에도 이를 실수할 수 있다.
 	Cut soldier_cut;
-	soldier_cut.setStyle("Soldier").setFront(1).setBack(1).setSide(1);
+	soldier_cut.setCut(Cut::Builder().setFront(1).setBack(1).setSide(1).build());
 	return 0;
 }
